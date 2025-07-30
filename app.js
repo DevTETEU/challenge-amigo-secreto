66f06207844e92612f9e88b2092241d5755e70ed
DevTeteu <dev.teteu@gmail.com>
2025-07-30 19:37:03 -0300

    Função para agregar nomes na lista

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 let amigos = [];
+
+function adicionarAmigo(){
+    let nome = document.getElementById('amigo').value;
+    if (nome){
+        amigos.push(nome);
+        document.getElementById('amigo').value = "";
+        atualizarLista(); // Atualiza a lista na tela
+    }
+}
+
+function atualizarLista(){
+    const mostrar = document.getElementById('toggleLista').checked;
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    
+}
